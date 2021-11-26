ac2df043985c0dafc932eace183e98229586c232
chepura <volodymyr.chepura.ki.2019@lpnu.ua>
2021-11-26 20:55:38 +0200

    registration

---
+++ web/js/register.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('registerBtn')
+
+button.onclick = (e) => {
+    e.preventDefault()
+    const login = document.getElementById('login').value
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('ConfirmPassword').value
+    const firstName = document.getElementById('firstName').value
+    const lastName = document.getElementById('lastName').value
+}
